31bdc1f08579386e1f11556e9e1385be41555f6a
linhnvh15vn <linhnvh15vn@gmail.com>
2024-07-29 22:48:00 +0700

    refactor: seperate constants and enums

---
+++ src/constants/enum.ts
@@ -0,0 +1,11 @@
+export enum QueryKey {
+  TOURS = 'tours',
+  USERS = 'users',
+}
+
+export enum UserRole {
+  USER = 'user',
+  GUIDE = 'guide',
+  LEAD_GUIDE = 'lead-guide',
+  ADMIN = 'admin',
+}
