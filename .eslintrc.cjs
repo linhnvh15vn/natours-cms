ff6259be36cdc25190481fd800d35018bc6b3328
linhnvh15vn <linhnvh15vn@gmail.com>
2024-07-30 14:03:12 +0700

    fix: make eslint run well

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,16 +2,17 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
     'plugin:@tanstack/eslint-plugin-query/recommended',
-    'plugin:import/recommended',
-    'plugin:import/typescript',
+    'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', '@typescript-eslint', 'import'],
+  parserOptions: {
+    project: './tsconfig.app.json',
+  },
+  plugins: ['react-refresh', 'import', '@typescript-eslint'],
   settings: {
     'import/resolver': {
       typescript: true,
